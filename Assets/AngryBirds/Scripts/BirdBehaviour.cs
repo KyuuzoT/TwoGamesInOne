ad667a4530da842ecddfd897d1cbddf49ad2a0c4
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2021-02-01 11:20:06 +0300

    ANGRY_BIRDS: Terminal suspension

--- Assets/AngryBirds/Scripts/BirdBehaviour.cs
+++ Assets/AngryBirds/Scripts/BirdBehaviour.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BirdBehaviour : MonoBehaviour
+namespace AngryBirds.Game.Scripts.Actor
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BirdBehaviour : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Rigidbody2D birdRB;
+        [SerializeField] private float releaseTime { get; set; } = 0.01f;
+        private bool isDragging { get; set; } = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Update()
+        {
+            if (isDragging)
+            {
+                birdRB.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+        }
+
+        private void OnMouseDown()
+        {
+            birdRB.isKinematic = true;
+            isDragging = true;
+        }
+
+        private void OnMouseUp()
+        {
+            birdRB.isKinematic = false;
+            isDragging = false;
+
+            StartCoroutine(ReleaseBird());
+        }
+
+        private IEnumerator ReleaseBird()
+        {
+            yield return new WaitForSeconds(releaseTime);
+            GetComponent<SpringJoint2D>().enabled = false;
+            isInAir = true;
+        }
     }
 }
+

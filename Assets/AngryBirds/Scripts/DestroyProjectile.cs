334aafc52a973c34847ee4cb105271a25dba2331
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2021-02-16 17:35:39 +0300

    ANGRYBIRDS: projectile death effect added

--- Assets/AngryBirds/Scripts/DestroyProjectile.cs
+++ Assets/AngryBirds/Scripts/DestroyProjectile.cs
@@ -7,10 +7,18 @@ namespace AngryBirds.Game.Scripts.Actor
 {
     public class DestroyProjectile : MonoBehaviour
     {
+        [SerializeField] private Transform deathEffect;
         [SerializeField] private float timeToDestroy = 3.0f;
+
         private void OnCollisionEnter2D(Collision2D collision)
         {
             Destroy(gameObject, timeToDestroy);
+        }
+
+        private void OnDestroy()
+        {
+            var effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+            effect.GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingOrder = gameObject.GetComponent<Renderer>().sortingOrder;
         }
     }
 }
